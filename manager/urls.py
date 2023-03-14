2fee13ff2267c700b4866b5bb7d4ded6c388bf56
SteppaCodes <steppacodes@gmail.com>
2023-03-14 01:29:30 +0100

    worked on the the Company-page &Project page

--- manager/urls.py
+++ manager/urls.py
@@ -5,8 +5,8 @@ from .views import *
 
 
 urlpatterns = [
-    path('',home,name='home'),
     path('company/<str:id>', companypage, name='company-page'),
+    path('project/<str:id>', ProjectPage, name='project-page'),
 ]
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
--- manager/views.py
+++ manager/views.py
@@ -5,9 +5,6 @@ from django.utils import timezone
 
 
 
-def home(request):
-    return render(request,'manager/home.html')
-
 def companypage(request,id):
     company = Company.objects.get(id=id)
     workers = company.worker_set.all()
@@ -30,3 +27,61 @@ def companypage(request,id):
                 "due_in":due_in
                   }
     return render(request,'manager/company-page.html', context)
+
+def ProjectPage(request, id):
+    project = Project.objects.get(id=id)
+    milestones = project.milestone_set.all()
+    deadline = project.deadline
+    deadline = timezone.localtime(deadline)
+    count = milestones.count()
+
+    if deadline:
+        due_in = int((deadline.date() - TODAY).days)
+        project.due_in = due_in
+
+    if request.user.in_company:
+        teams = project.teams.all()
+        workers = []
+        project.progress = 0
+
+        #Getting Each worker in departments that are working on the project
+        for team in teams:
+            for worker in team.workers.all():
+                if worker not in workers:
+                    workers.append(worker)
+
+        #Creating a value for each milestone
+        #this will affect the project progress - if all milestones are complete
+        #the project progress = 100% 
+        if count > 0:
+            value = 100 / count
+        #Assigning the value to each milestone
+        for milestone in milestones:
+            milestone.value = int(value)
+            #Increasing the value of project progress if the milestone is completed
+            if milestone.status == "Completed":
+                project.progress += milestone.value
+                project.save()
+
+        context = {"project":project,
+                   'milestones':milestones,
+                    'count':count,
+                    'teams':teams, 
+                    "workers":workers,
+                    'due_in':due_in }
+    else:
+        if count > 0:
+            value = 100 / count
+
+        for milestone in milestones:
+            milestone.value = int(value)
+            #Increasing the value of project progress if the milestone is completed
+            if milestone.status == "Completed":
+                project.progress += milestone.value
+                project.save()
+
+        context =  {"milestones": milestones,
+                     "project": project,
+                     'due_in':due_in,}   
+
+    return render(request,'manager/project-page.html', context)
