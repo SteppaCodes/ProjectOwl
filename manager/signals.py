28c7d381e1b458bb2a3b79d35a4360e8e03bfccf
SteppaCodes <steppacodes@gmail.com>
2023-03-14 22:33:15 +0100

    Crud operations for project & milestone

--- manager/signals.py
+++ manager/signals.py
@@ -6,7 +6,27 @@ from users.models import CostumUser
 
 @receiver(post_save,sender= CostumUser)
 def create_work_profile(sender,instance,**Kwargs):
-    print("Triggered")
+
     if instance.in_company:
         Worker.objects.get_or_create(user=instance)
-        print("worker")
+
+@receiver(post_save,sender= Project)
+def create_project_activity(sender,created,instance,**Kwargs):
+    if created:
+        if instance.created_by.in_company:
+            activity = Activity.objects.create(
+                user= instance.created_by,
+                project= instance,
+                message =  "created a new project ",
+                company=instance.created_by.worker.company
+            )
+            print(activity.message)
+    else:
+        if instance.created_by.in_company:
+            activity = Activity.objects.create(
+                user= instance.updated_by,
+                project= instance,
+                message =  "Updated",
+                company=instance.created_by.worker.company
+            )
+            print(activity.message)
--- manager/urls.py
+++ manager/urls.py
@@ -15,9 +15,17 @@ urlpatterns = [
     path('company/<str:id>', companypage, name='company-page'),
     path('user-dashboard/<str:id>',userdashboard ,name='user-dashboard'),
 
+    path('create-project/', createproject, name='create-project'),
+    path('create-milestone/<str:id>', createmilestone, name='create-milestone'),
+
     path('project/<str:id>', ProjectPage, name='project-page'),
 
-    
+    path('update-project/<str:id>', updateproject,name='update-project'),
+    path('update-milestone/<str:id>', updatemilestone,name='update-milestone'),
+
+    path('delete-project/<str:id>', deleteproject, name='delete-project'),
+    path('delete-milestone/<str:id>', deletemilestone, name='delete-milestone'),
+
 ]
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
