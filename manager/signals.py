21fc64c3f89dfb5ce52b213b16bee3fd0ea6a3a1
SteppaCodes <steppacodes@gmail.com>
2023-03-15 16:06:32 +0100

    Activity display1

--- manager/signals.py
+++ manager/signals.py
@@ -1,7 +1,8 @@
-from django.db.models.signals import pre_save, post_save
+from django.db.models.signals import pre_save, post_save,pre_delete
 from django.dispatch import receiver
-from .models import Project,Worker, MileStone,Activity
+from .models import Project,Worker
 from users.models import CostumUser
+from datetime import datetime, timedelta
 
 
 @receiver(post_save,sender= CostumUser)
@@ -10,23 +11,3 @@ def create_work_profile(sender,instance,**Kwargs):
     if instance.in_company:
         Worker.objects.get_or_create(user=instance)
 
-@receiver(post_save,sender= Project)
-def create_project_activity(sender,created,instance,**Kwargs):
-    if created:
-        if instance.created_by.in_company:
-            activity = Activity.objects.create(
-                user= instance.created_by,
-                project= instance,
-                message =  "created a new project ",
-                company=instance.created_by.worker.company
-            )
-            print(activity.message)
-    else:
-        if instance.created_by.in_company:
-            activity = Activity.objects.create(
-                user= instance.updated_by,
-                project= instance,
-                message =  "Updated",
-                company=instance.created_by.worker.company
-            )
-            print(activity.message)
