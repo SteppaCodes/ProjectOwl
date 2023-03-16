5e91d2966818eeb1c54bb4e2960e2bfd57562b83
SteppaCodes <steppacodes@gmail.com>
2023-03-16 16:32:16 +0100

    Minor changes

--- manager/forms.py
+++ manager/forms.py
@@ -1,52 +1,18 @@
-# from django.forms import ModelForm
-# from . models import Team,Company
+from django.forms import ModelForm
+from . models import Team,Company
 
-# class CreateCompanyForm(ModelForm):
+class CreateCompanyForm(ModelForm):
+    class Meta:
+        model = Company 
+        fields= '__all__'
+        exclude = ['owner']
+
+class TeamForm(ModelForm):
+    class Meta:
+        model = Team
+        fields = ["name", "head", "workers"]
+
+# class RoleForm(ModelForm):
 #     class Meta:
-#         model = Company 
-#         fields= '__all__'
-#         exclude = ['owner']
-
-# class MileStoneForm(ModelForm):
-#     class Meta:
-#         model = MileStone
-#         fields = "__all__"
-#         exclude = ['created_by', 'status','complete', 'project', 'value']
-
-# class TeamForm(ModelForm):
-#     class Meta:
-#         model = Team
-#         fields = ["name", "head", "workers"]
-
-# # class RoleForm(ModelForm):
-# #     class Meta:
-# #         model = Role
-# #         fields = ["title"]
-
-# class PersonalProjectForm(ModelForm):
-#     class Meta:
-#         model = Project
-#         fields =   ['name','description','deadline']
-       
-
-# class CompanyProjectForm(ModelForm):
-
-#     class Meta:
-#         model = Project
-#         fields =   ['name','description','deadline', 'teams',]
-        
-# class JoinCompanyForm(ModelForm):
-#     class Meta:
-#         model = Company
-#         fields = ['company_key']
-
-
-# class TaskForm(ModelForm):
-#     class Meta:
-#         model = Task
-#         fields =['name', 'workers']
-
-# class AttachmentForm(ModelForm):
-#     class Meta:
-#         model = Task
-#         fields =['name', 'file']
+#         model = Role
+#         fields = ["title"]
